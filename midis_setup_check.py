03a7417afd26a4c489e3a3461255d386afd706db
MidisAdmin <pierce@mymidis.com>
2026-07-10 18:50:50 -0700

    Update Wifi Splash

--- midis_setup_check.py
+++ midis_setup_check.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import os
+import time
 
 def is_wifi_configured():
     try:
@@ -10,7 +11,13 @@ def is_wifi_configured():
         return False
 
 def start_hotspot():
-    # Create hostapd config
+    # Unblock WiFi
+    subprocess.run(['sudo', 'rfkill', 'unblock', 'wifi'])
+    
+    # Stop NetworkManager from managing wlan0
+    subprocess.run(['sudo', 'nmcli', 'device', 'disconnect', 'wlan0'], capture_output=True)
+    
+    # Write hostapd config
     subprocess.run(['sudo', 'bash', '-c', '''
 cat > /etc/hostapd/hostapd.conf << EOF
 interface=wlan0
@@ -23,8 +30,8 @@ auth_algs=1
 ignore_broadcast_ssid=0
 EOF
 '''])
-    
-    # Create dnsmasq config
+
+    # Write dnsmasq config
     subprocess.run(['sudo', 'bash', '-c', '''
 cat > /etc/dnsmasq.conf << EOF
 interface=wlan0
@@ -33,12 +40,16 @@ address=/#/192.168.4.1
 EOF
 '''])
 
-    # Set static IP for wlan0
+    # Set static IP
+    subprocess.run(['sudo', 'ip', 'addr', 'flush', 'dev', 'wlan0'])
     subprocess.run(['sudo', 'ip', 'addr', 'add', '192.168.4.1/24', 'dev', 'wlan0'])
+    subprocess.run(['sudo', 'ip', 'link', 'set', 'wlan0', 'up'])
     
-    # Start services
-    subprocess.run(['sudo', 'systemctl', 'start', 'hostapd'])
-    subprocess.run(['sudo', 'systemctl', 'start', 'dnsmasq'])
+    # Start hostapd and dnsmasq
+    subprocess.run(['sudo', 'systemctl', 'restart', 'hostapd'])
+    time.sleep(2)
+    subprocess.run(['sudo', 'systemctl', 'restart', 'dnsmasq'])
+    time.sleep(1)
     
     # Start the setup portal
     subprocess.run(['sudo', 'python3', '/home/pi/Midis_1.0/midis_setup_portal.py'])
