daa16a0cd687138aea94c5ff6ef5dacf2a7bdc5b
MidisAdmin <pierce@mymidis.com>
2026-07-11 22:24:43 -0700

    Fix up art py

--- midis_art.py
+++ midis_art.py
@@ -1,21 +1,27 @@
 import time
 import random
+import os
 from PIL import Image
 from rgbmatrix import graphics
 
-IMAGES = [
-    "/usr/local/share/midis-icons/starrynight.png",
-    "/usr/local/share/midis-icons/framed.png",
-    "/usr/local/share/midis-icons/house.png",
-    "/usr/local/share/midis-icons/moonrise.png",
-]
+ART_DIR = "/home/pi/Midis_1.0/icons/midis-icons/art"
 
-current_image = random.randint(0, len(IMAGES) - 1)
+IMAGES = [
+    os.path.join(ART_DIR, f)
+    for f in os.listdir(ART_DIR)
+    if f.lower().endswith('.png')
+] if os.path.exists(ART_DIR) else []
+
+current_image = random.randint(0, max(len(IMAGES) - 1, 0)) if IMAGES else 0
 last_switch = 0
 IMAGE_DURATION = 30
 
 def draw(canvas, font, small_font):
     global current_image, last_switch
+
+    if not IMAGES:
+        graphics.DrawText(canvas, small_font, 2, 16, graphics.Color(255, 0, 0), "No art")
+        return
 
     now = time.time()
     if now - last_switch > IMAGE_DURATION:
