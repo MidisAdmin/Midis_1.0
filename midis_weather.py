61bb000f42232868d3f1d6729b5a18d623ee868e
MidisAdmin <pierce@mymidis.com>
2026-07-12 19:34:00 -0700

    show moon instead of sun at night

--- midis_weather.py
+++ midis_weather.py
@@ -34,7 +34,9 @@ def temp_color(temp):
     else: return (255, 30, 0)
 
 def code_to_icon(code):
-    if code == 0: return "sun"
+    if code == 0:
+        t = time.localtime()
+        return "moon" if t.tm_hour >= 22 or t.tm_hour < 5 else "sun"
     elif code in [1, 2, 3]: return "cloud"
     elif code in [45, 48]: return "fog"
     elif code in [51,53,55,61,63,65,80,81,82]: return "rain"
